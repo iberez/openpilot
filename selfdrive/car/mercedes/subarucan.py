71f9f7399af8f1391cca1287ad19e3ec2ff3c452
Isaac Berez <isaacberez@MacBook-Pro.localdomain>
2019-10-19 16:49:24 -0700

    updated subarucan.py by replacing IMPREZA with ECLASS and subaru with mercedes

--- selfdrive/car/mercedes/subarucan.py
+++ selfdrive/car/mercedes/subarucan.py
@@ -1,16 +1,16 @@
 import copy
 from cereal import car
-from selfdrive.car.subaru.values import CAR
+from selfdrive.car.mercedes.values import CAR
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
-def subaru_checksum(packer, values, addr):
+def mercedes_checksum(packer, values, addr):
   dat = packer.make_can_msg(addr, 0, values)[2]
   return (sum(dat[1:]) + (addr >> 8) + addr) & 0xff
 
 def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_step):
 
-  if car_fingerprint == CAR.IMPREZA:
+  if car_fingerprint == CAR.ECLASS:
     #counts from 0 to 15 then back to 0 + 16 for enable bit
     idx = ((frame // steer_step) % 16)
 
@@ -20,15 +20,15 @@ def create_steering_control(packer, car_fingerprint, apply_steer, frame, steer_s
       "LKAS_Request": 1 if apply_steer != 0 else 0,
       "SET_1": 1
     }
-    values["Checksum"] = subaru_checksum(packer, values, 0x122)
+    values["Checksum"] = mercedes_checksum(packer, values, 0x122)
 
   return packer.make_can_msg("ES_LKAS", 0, values)
 
 def create_steering_status(packer, car_fingerprint, apply_steer, frame, steer_step):
 
-  if car_fingerprint == CAR.IMPREZA:
+  if car_fingerprint == CAR.ECLASS:
     values = {}
-    values["Checksum"] = subaru_checksum(packer, {}, 0x322)
+    values["Checksum"] = mercedes_checksum(packer, {}, 0x322)
 
   return packer.make_can_msg("ES_LKAS_State", 0, values)
 
@@ -38,7 +38,7 @@ def create_es_distance(packer, es_distance_msg, pcm_cancel_cmd):
   if pcm_cancel_cmd:
     values["Main"] = 1
 
-  values["Checksum"] = subaru_checksum(packer, values, 545)
+  values["Checksum"] = mercedes_checksum(packer, values, 545)
 
   return packer.make_can_msg("ES_Distance", 0, values)
 
@@ -51,6 +51,6 @@ def create_es_lkas(packer, es_lkas_msg, visual_alert, left_line, right_line):
   values["LKAS_Left_Line_Visible"] = int(left_line)
   values["LKAS_Right_Line_Visible"] = int(right_line)
 
-  values["Checksum"] = subaru_checksum(packer, values, 802)
+  values["Checksum"] = mercedes_checksum(packer, values, 802)
 
   return packer.make_can_msg("ES_LKAS_State", 0, values)
